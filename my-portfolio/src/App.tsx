0ed1bda5db40b5b1d6ffc9828032b975a316bbf1
Hansama Dewindi Rajaguru <141362143+HansamaRajaguru@users.noreply.github.com>
2024-09-16 07:58:19 +0530

    initialized my-portfolio

---
+++ my-portfolio/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header>
+        <h1>Your Name</h1>
+        <nav>
+          <ul>
+            <li><a href="#about">About Me</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      
+    </div>
+  );
+}
+
+export default App;
