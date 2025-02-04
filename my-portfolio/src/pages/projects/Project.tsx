7086e408ffe0c08b80a0b4b23b7dc6e343ed3ad8
Hansama Dewindi Rajaguru <141362143+HansamaRajaguru@users.noreply.github.com>
2025-02-04 10:44:26 +0530

    Fixed image paths and moved images to public folder

--- my-portfolio/src/pages/projects/Project.tsx
+++ my-portfolio/src/pages/projects/Project.tsx
@@ -16,23 +16,24 @@ const projectDetails: ProjectDetails[] = [
         title: "RecipeHub Web Application",
         description: "A recipe-sharing platform allowing users to browse, share, and manage their favorite recipes using the MERN stack.",
         duration: "2024",
-        image: process.env.PUBLIC_URL + "/Hansama-Rajaguru/images/RecipeHub-Web.png"
+        image: process.env.PUBLIC_URL + "/images/RecipeHub-Web.png"
     },
     {
         id: 2,
         title: "Portfolio Website",
         description: "A personal portfolio website showcasing projects, skills, and achievements. Developed using React and CSS.",
         duration: "2024",
-        image: process.env.PUBLIC_URL + "/Hansama-Rajaguru/images/Portfolio.png",
+        image: process.env.PUBLIC_URL + "/images/Portfolio.png"
     },
     {
         id: 3,
         title: "Botanic Buddy Mobile Application",
         description: "A plant care and disease identification mobile application using React-Native and TypeScript.",
         duration: "2023",
-        image: process.env.PUBLIC_URL + "/Hansama-Rajaguru/images/mobileapp.png",
+        image: process.env.PUBLIC_URL + "/images/mobileapp.png"
     }
 ];
+
 
 const Projects: React.FC = () => {
     return (
